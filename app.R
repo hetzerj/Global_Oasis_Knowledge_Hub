6c9f43933ae186f0ce64a81e236272c9d2b413c5
Jessica Hetzer <jessica.hetzer@senckenberg.de>
2025-02-06 16:18:20 +0100

    Init for tabPanels and styles

--- app.R
+++ app.R
@@ -1,51 +1,30 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  navbarPage("Global Oasis Knowledge Hub",
+             tabPanel("Home",
+                      div(class = "container",
+                          div(class = "text-box",
+                              h1("Global Oasis Knowledge Hub"),
+                              h3("A Senckenberg initiative for leveraging knowledge supporting science and policy.")
+                          )
+                      )
+             ),
+             tabPanel("About",
+                      div(class = "content", h2("About"), p("This page will contain information about the initiative."))
+             ),
+             tabPanel("Knowledge Hub",
+                      div(class = "content", h2("Knowledge Hub"), p("A collection of valuable resources and insights."))
+             ),
+             tabPanel("Credits",
+                      div(class = "content", h2("Credits"), p("Acknowledgments and contributors to this initiative."))
+             )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) {}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
-
-# Run the application 
 shinyApp(ui = ui, server = server)
