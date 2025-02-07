7078b45537ebf66520b33df54ebeab1611fec606
Jessica Hetzer <jessica.hetzer@senckenberg.de>
2025-02-07 19:12:06 +0100

    Included Credits tabs with pictures

--- app.R
+++ app.R
@@ -5,35 +5,156 @@ ui <- fluidPage(
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
   ),
-  navbarPage("Global Oasis Knowledge Hub",
-             tabPanel("Home",
-                      div(class = "container",
+  navbarPage(
+    "Global Oasis Knowledge Hub",
+    tabPanel("Home", div(
+      class = "container",
+      
+      tags$img(src = "SNG_Logo_transperant.png", class = "logo"),
+      
+      div(
+        class = "text-box",
+        h1("Global Oasis Knowledge Hub"),
+        h3(
+          "A Senckenberg initiative for leveraging knowledge supporting science and policy."
+        )
+      )
+    )),
+    tabPanel("About", div(
+      class = "content",
+      h2("About"),
+      p("This page will contain information about the initiative.")
+    )),
+    tabPanel("Knowledge Hub", div(
+      class = "content",
+      h2("Knowledge Hub"),
+      p("A collection of valuable resources and insights.")
+    )), 
+    tabPanel("Credits",
+             
+             # Team Section
+             fluidRow(
+               column(12, h2("Team", align = "center")),
+               
+               column(3, align = "center",
+                      img(src = "Jessica_Hetzer.jpeg",  class = "team-img"),
+                      p("Jessica Hetzer", style="font-weight: bold"),
+                      p("Lead Developer & Concept Creator")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "Aidin_Niamir.jpeg",  class = "team-img"),
+                      p("Aidin Niamir", style="font-weight: bold"),
+                      p("Coordinator & Concept Creator")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "Rainer_Krug.jpeg", class = "team-img"),
+                      p("Rainer M. Krug", style="font-weight: bold"),
+                      p("Software Developer")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "Empty_profile.png",  class = "team-img"),
+                      p("Mechthilde Falkenhahn", style="font-weight: bold"),
+                      p("Literature review")
+               )
+             ),
+             
+             hr(),
+             
+             # Advisory Board Section
+             # Advisory Board Section
+             fluidRow( style="background-color: rgb(160, 177, 203);",
+               column(12, h3("Advisory Members", align = "center")),
+               
+               column(12, align = "center",
+                      div(style="display: flex; justify-content: center; gap: 30px;",
+                          column(3, align = "center",
+                                 img(src = "Klement_Tockner.jpeg",  class = "advisor-img"),
+                                 p("Klement Tockner", style="font-weight: bold")
+                          ),
                           
-                          tags$img(src = "SNG_Logo_transperant.png", class = "logo"),
-                          
-                          div(class = "text-box",
-                              h1("Global Oasis Knowledge Hub"),
-                              h3("A Senckenberg initiative for leveraging knowledge supporting science and policy.")
+                          column(3, align = "center",
+                                 img(src = "Jonathan_Jeschke.jpeg", class = "advisor-img"),
+                                 p("Jonathan Jeschke", style="font-weight: bold")
                           )
                       )
+               )
              ),
-             tabPanel("About",
-                      div(class = "content", h2("About"), p("This page will contain information about the initiative."))
+             
+             fluidRow( style="background-color: rgb(160, 177, 203);gap:10px",
+               column(12, h3("Advisory Board", align = "center")),
+               
+               column(12, align = "center",
+                      div(style="display: flex; justify-content: center; gap: 20px;",
+                          column(2, align = "center",
+                                 img(src = "Empty_profile.png", class = "advisor-img"),
+                                 p("Advisor 1", style="font-weight: bold")
+                          ),
+                          
+                          column(2, align = "center",
+                                 img(src = "Empty_profile.png", class = "advisor-img"),
+                                 p("Advisor 2", style="font-weight: bold")
+                          ),
+                          
+                          column(2, align = "center",
+                                 img(src = "Empty_profile.png", class = "advisor-img"),
+                                 p("Advisor 3", style="font-weight: bold")
+                          ),
+                          
+                          column(2, align = "center",
+                                 img(src = "Empty_profile.png", class = "advisor-img"),
+                                 p("Advisor 4", style="font-weight: bold")
+                          ),
+                          
+                          column(2, align = "center",
+                                 img(src = "Empty_profile.png", class = "advisor-img"),
+                                 p("Advisor 5", style="font-weight: bold")
+                          )
+                      )
+               )
              ),
-             tabPanel("Knowledge Hub",
-                      div(class = "content", h2("Knowledge Hub"), p("A collection of valuable resources and insights."))
-             ),
-             tabPanel("Credits",
-                      div(class = "content", h2("Credits"), p("Acknowledgments and contributors to this initiative."))
+             hr(),
+             
+             # Partners Section
+             fluidRow(
+               column(12, h2("Partners", align = "center")),
+               
+               column(3, align = "center",
+                      img(src = "UNCCD.jpeg", class = "partner-logo-img"),
+                      br(),
+                      a("Visit Partner", href = "https://partner1.com", target="_blank")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "Ramsar.jpeg", class = "partner-logo-img"),
+                      br(),
+                      a("Visit Partner", href = "https://partner2.com", target="_blank")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "unesco_cairo.png", class = "partner-logo-img"),
+                      br(),
+                      a("Visit Partner", href = "https://partner3.com", target="_blank")
+               ),
+               
+               column(3, align = "center",
+                      img(src = "Empty_partner.png",  class = "partner-logo-img"),
+                      br(),
+                      a("Visit Partner", href = "https://partner4.com", target="_blank")
+               )
              )
+    )
+    
   )
 )
 
+
 server <- function(input, output) {
-  
   # 1. Define seed references and save metadata --------------------------------
-  # In this step, a predefined set of seed references is used as the starting point. 
-  # The metadata of these references is retrieved from the open-source OpenAlex database. 
+  # In this step, a predefined set of seed references is used as the starting point.
+  # The metadata of these references is retrieved from the open-source OpenAlex database.
   
   # Set update flags to FALSE to prevent unnecessary reloading of data
   seed_knowledge_update <- FALSE
@@ -44,19 +165,21 @@ server <- function(input, output) {
   
   # Check if the metadata file already exists and no updates are required
   # If the file exists and updates are not needed, load the existing file instead of fetching new data
-  if (file.exists(fn) & (!seed_knowledge_update)){
+  if (file.exists(fn) & (!seed_knowledge_update)) {
     seed_works <- readRDS(fn)
-  } 
+  }
   else {
     # Read predefined literature IDs from a CSV file
-    seed_df <- read.csv(file.path(".", "input", "oasis_seed_references.csv"), header = TRUE, sep = "\t")
+    seed_df <- read.csv(
+      file.path(".", "input", "oasis_seed_references.csv"),
+      header = TRUE,
+      sep = "\t"
+    )
     
     # Retrieve metadata for the seed references from OpenAlex
-    seed_works <- oa_fetch(
-      entity = "works",
-      id = seed_df$oa_id,
-      verbose = FALSE
-    )
+    seed_works <- oa_fetch(entity = "works",
+                           id = seed_df$oa_id,
+                           verbose = FALSE)
     
     # Save the retrieved metadata for future use
     saveRDS(seed_works, fn)
@@ -64,23 +187,21 @@ server <- function(input, output) {
   
   # 2. Expand references by exploring citations from and to seed references -----------------
   # This step performs a snowball search by collecting all citations both from and to the seed references.
-
+  
   # Define the file path where the expanded reference metadata should be stored
   fn <- file.path("data", "expanded_knowledge_works.rds")
-
+  
   # If the expanded reference file exists, load the saved data; otherwise, perform the expansion search
-  if (file.exists(fn) & (!seed_knowledge_update) & (!expanded_knowledge_update)) {
+  if (file.exists(fn) &
+      (!seed_knowledge_update) & (!expanded_knowledge_update)) {
     expanded_works <- readRDS(fn)
   }
   else {
     # Perform a snowball search to retrieve citations related to the seed references
-    expanded_works <- oa_snowball(
-      identifier = seed_works$id,
-      verbose = FALSE
-    )
+    expanded_works <- oa_snowball(identifier = seed_works$id, verbose = FALSE)
     # Add a new field `is_seed` indicating whether a reference was one of the original seed references
     expanded_works$nodes$is_seed <- expanded_works$nodes$id %in% openalexR:::shorten_oaid(seed_works$id)
-
+    
     # Add short names for authors to enhance readability
     expanded_works$nodes$authors_short <- sapply(expanded_works$nodes$author, function(authors) {
       if (is.data.frame(authors)) {
