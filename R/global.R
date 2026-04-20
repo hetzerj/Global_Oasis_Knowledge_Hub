9ae3ab42c29dfbd9d60f0560f7c8f1c13ab4b7b3
Jessica Hetzer <jessica.hetzer@senckenberg.de>
2026-04-20 15:11:02 +0200

    Add R-code line in global.R to create subfolder "Overview_figures" if its not there yet

--- R/global.R
+++ R/global.R
@@ -12,6 +12,8 @@ GOKH_version="0.0"
 # shinyapps.io: use a writable temp folder
 CACHE_DIR <- file.path(tempdir(), "zenodo_cache")
 
+dir.create("www/Overview_figures", showWarnings = FALSE, recursive = TRUE)
+
 col_oasis <- c(rgb(60/255, 77/255, 103/255), rgb(160/255, 177/255, 203/255), "#50521A", "#A19658","#794839", "#D1B091")
 col_water_scale <- colorRampPalette(col_oasis[2:1])
 col_green_scale <- colorRampPalette(col_oasis[4:3])
